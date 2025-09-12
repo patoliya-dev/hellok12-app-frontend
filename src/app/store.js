e4385e91646df0cb7e505f594ea98b472c322700
Amy M <am.patoliya@patoliyainfotech.com>
2025-09-12 19:02:33 +0530

    Remove booking in store

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice';
-import bookingsReducer from "../features/bookings/bookingsSlice";
-// Existing...
 import progressReducer from '../features/progress/progressSlice';
 import lessonsReducer from '../features/lessons/lessonsSlice';
 
@@ -10,7 +8,6 @@ import lessonsReducer from '../features/lessons/lessonsSlice';
 const store = configureStore({
   reducer: {
     auth: authReducer,
-    bookings: bookingsReducer,
     progress: progressReducer,
     lessons: lessonsReducer,
   },
