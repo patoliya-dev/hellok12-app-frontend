4ed5eafd8978891c669478affb2859aabd0b9ebf
Amy M <am.patoliya@patoliyainfotech.com>
2025-12-04 10:34:25 +0530

    feat: Auto-select the first child for parent users by ensuring child data is fetched and integrating the selection hook into protected routes.

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -2,6 +2,7 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { selectAuthUser } from "reducers/auth/authSelectors";
 import { DEFAULT_ROUTES } from "../utils/constant";
+import useAutoSelectChild from "../hooks/useAutoSelectChild";
 
 export default function ProtectedRoute({ allowedRoles }) {
   const currentUser = useSelector(selectAuthUser);
@@ -11,8 +12,13 @@ export default function ProtectedRoute({ allowedRoles }) {
   }
 
   const isAuthorized = allowedRoles.includes(currentUser.role);
+  useAutoSelectChild();
 
   // If authorized, the <Outlet> will render the matching child route.
   // Otherwise, we redirect.
-  return isAuthorized ? <Outlet /> : <Navigate to={DEFAULT_ROUTES[currentUser.role] || "/"} replace />;
+  return isAuthorized ? (
+    <Outlet />
+  ) : (
+    <Navigate to={DEFAULT_ROUTES[currentUser.role] || "/"} replace />
+  );
 }
--- src/hooks/useAutoSelectChild.js
+++ src/hooks/useAutoSelectChild.js
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { selectAuthUser } from "reducers/auth/authSelectors";
 import { selectStudent } from "reducers/profile/profileSlice";
+import { fetchCurrentUser } from "reducers/auth/authThunks";
 
 /**
  * Custom hook to automatically select the first child for parent users
@@ -12,17 +13,28 @@ const useAutoSelectChild = () => {
   const selectedChildId = useSelector((state) => state.profile.selectedChildId);
 
   useEffect(() => {
-    if (
-      authUser?.role === "parent" &&
-      authUser?.profile?.children?.length > 0
-    ) {
-      if (!selectedChildId) {
+    const initializeChildSelection = async () => {
+      if (
+        authUser?.role === "parent" &&
+        (!authUser?.profile?.children || authUser.profile.children.length === 0)
+      ) {
+        await dispatch(fetchCurrentUser());
+        return;
+      }
+
+      if (
+        authUser?.role === "parent" &&
+        authUser?.profile?.children?.length > 0 &&
+        !selectedChildId
+      ) {
         const firstChildId =
           authUser.profile.children[0]._id || authUser.profile.children[0].id;
         dispatch(selectStudent(firstChildId));
       }
-    }
-  }, [authUser, selectedChildId, dispatch]);
+    };
+
+    initializeChildSelection();
+  }, [authUser?.role, authUser?.profile?.children, selectedChildId, dispatch]);
 };
 
 export default useAutoSelectChild;
